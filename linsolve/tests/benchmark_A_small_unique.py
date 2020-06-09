c8a7dcb845b2dbce9a1f74419ba330c8678ac51c
Aaron Parsons <aparsons@berkeley.edu>
2020-06-09 14:33:18 -0700

    Added some benchmarks for speed testing

---
+++ linsolve/tests/benchmark_A_small_unique.py
@@ -0,0 +1,29 @@
+import linsolve
+import numpy as np
+import time, random
+
+np.random.seed(0)
+
+NPRMS = 10
+NEQS = 100
+SIZE = 100000
+MODE = 'solve'
+
+prms = {'g%d' % i: np.arange(SIZE) for i in range(NPRMS)}
+prm_list = list(prms.keys())
+prms['c0'] = np.arange(SIZE)
+
+eqs = [('+c0*'.join(['%s'] * 5))  % tuple(random.sample(prm_list, 5)) 
+            for i in range(NEQS)]
+
+data = {eq: eval(eq, prms) for eq in eqs}
+
+ls = linsolve.LinearSolver(data, c0=prms['c0'])
+t0 = time.time()
+sol = ls.solve(mode=MODE)
+t1 = time.time()
+
+print('Solved in {}'.format(t1-t0))
+
+for k in prm_list:
+    assert np.mean(np.abs(sol[k] - prms[k])**2) < 1e-3
