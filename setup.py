81d90a176eedca80bb3b0fd664ccf9c3d5dd26f2
Tyler Cox <tyler.a.cox@berkeley.edu>
2021-11-04 17:45:10 -0700

    Update setup file

--- setup.py
+++ setup.py
@@ -6,28 +6,35 @@ import json
 sys.path.append("linsolve")
 import version
 
-data = [version.git_origin, version.git_hash, version.git_description, version.git_branch]
-with open(op.join('linsolve', 'GIT_INFO'), 'w') as outfile:
+data = [
+    version.git_origin,
+    version.git_hash,
+    version.git_description,
+    version.git_branch,
+]
+with open(op.join("linsolve", "GIT_INFO"), "w") as outfile:
     json.dump(data, outfile)
 
 setup_args = {
-    'name': 'linsolve',
-    'author': 'HERA Team',
-    'url': 'https://github.com/HERA-Team/linsolve',
-    'license': 'BSD',
-    'description': 'high-level tools for linearizing and solving systems of equations',
-    'package_dir' : {'linsolve':'linsolve'},
-    'packages' : ['linsolve'],
-    'version': version.version,
-    'include_package_data': True,
-    'install_requires': ['numpy>=1.14', 'scipy'],
-    'classifiers': ['Development Status :: 5 - Production/Stable',
-                    'Intended Audience :: Science/Research',
-                    'License :: OSI Approved :: BSD License',
-                    'Programming Language :: Python :: 3.6',
-                    'Topic :: Scientific/Engineering :: Mathematics'],
-    'keywords': 'linear equations optimal estimation',
+    "name": "linsolve",
+    "author": "HERA Team",
+    "url": "https://github.com/HERA-Team/linsolve",
+    "license": "BSD",
+    "description": "high-level tools for linearizing and solving systems of equations",
+    "package_dir": {"linsolve": "linsolve"},
+    "packages": ["linsolve"],
+    "version": version.version,
+    "include_package_data": True,
+    "install_requires": ["numpy>=1.14", "scipy", "tensorflow>=2.4.0"],
+    "classifiers": [
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: BSD License",
+        "Programming Language :: Python :: 3.6",
+        "Topic :: Scientific/Engineering :: Mathematics",
+    ],
+    "keywords": "linear equations optimal estimation",
 }
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     setup(**setup_args)
